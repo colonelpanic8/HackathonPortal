58e95a9eff6dc80114b45048d28b61d5c7f358d0
Ivan Malison <IvanMalison@gmail.com>
2013-03-21 13:47:23 -0700

    tabs -> spaces adding mocks.

--- hackathon_portal/models.py
+++ hackathon_portal/models.py
@@ -1,53 +1,50 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-import simplejson
 from sqlalchemy.types import TypeDecorator, VARCHAR
 from sqlalchemy.orm.exc import NoResultFound
 
 from . import app
-from . import common
 
 
 db = SQLAlchemy(app)
 db.Model.itercolumns = classmethod(lambda cls: cls.__table__.columns._data.iterkeys())
 
 
-class Project(db.model):
-
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(250), unique=True)
-	hackathon = db.Column(db.Integer, db.ForeignKey(Hackathon.id))
-	description = db.Column(db.String(5000))
-	link = db.Column(db.String(250))
-
-	def __init__(self, name="", hackathon=0, description="", link=""):
-		self.name = name
-		self.hackathon = hackathon
-		self.description = description
-		self.link = link
-
-
 class Person(db.model):
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(250))
-	yelp_id = db.Column(db.String(250))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    yelp_id = db.Column(db.String(250))
 
-	def __init__(self, name="", yelp_id=""):
-		self.name = name
-		self.yelp_id = yelp_id
+    def __init__(self, name="", yelp_id=""):
+        self.name = name
+        self.yelp_id = yelp_id
 
 
 class Hackathon(db.model):
 
-	id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+
+class Project(db.model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True)
+    hackathon = db.Column(db.Integer, db.ForeignKey(Hackathon.id))
+    description = db.Column(db.String(5000))
+    link = db.Column(db.String(250))
+
+    def __init__(self, name="", hackathon=0, description="", link=""):
+        self.name = name
+        self.hackathon = hackathon
+        self.description = description
+        self.link = link
 
 
 class ProjectToPerson(db.model):
 
-	id = db.Column(db.Integer, primary_key=True)
-	project_id = db.Column(db.Integer, db.ForeignKey(Project.id))
-	person_id = db.Column(db.Integer, db.ForeignKey(Person.id))
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey(Project.id))
+    person_id = db.Column(db.Integer, db.ForeignKey(Person.id))
 
-	def __init__(self, project_id, person_id):
-		self.project_id = project_id
-		self.person_id = person_id
+    def __init__(self, project_id, person_id):
+        self.project_id = project_id
+        self.person_id = person_id
