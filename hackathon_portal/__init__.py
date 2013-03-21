1b434c8e4ee165110797ac807473870c712dc1af
Ivan Malison <IvanMalison@gmail.com>
2013-03-21 12:43:53 -0700

    Updateds location of db.

--- hackathon_portal/__init__.py
+++ hackathon_portal/__init__.py
@@ -4,14 +4,15 @@ import os
 from flask import Flask, url_for
 
 application_directory = os.path.abspath(os.path.dirname(__file__))
+server_directory, _ = os.path.split(application_directory)
+print server_directory
 
 app = Flask(__name__)
 app.jinja_env.globals['get_static_url'] = lambda filename: url_for('static', filename=filename)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL',
-    'sqlite:///{app_dir}/testing.db'.format(
-        app_dir=application_directory
+    'sqlite:///{app_dir}/hackathon_portal.db'.format(
+        app_dir=server_directory
     )
 )
 app.debug = True
-
