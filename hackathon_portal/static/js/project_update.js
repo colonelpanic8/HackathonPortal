570c71772d401ffe6bf5a8480c707c3db66c8589
Jon Yee Chuah <jonyee90@gmail.com>
2013-03-22 00:35:07 -0700

    autocomplete working without proper list

--- hackathon_portal/static/js/project_update.js
+++ hackathon_portal/static/js/project_update.js
@@ -3,5 +3,35 @@ $(document).ready(function() {
         	target = '#'+ element.target.getAttribute('value');
        		$(target).slideToggle();
   	}); 
+
+	$(function() {
+		var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    	];
+		$( "#member-input" ).autocomplete({
+			source: availableTags
+		});
+	});
 });
 
