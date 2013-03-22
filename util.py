c3bde4ce039b8482fafdeb76e31bd233a22fc613
Srivatsan Sridharan <ssridhar@sfoengwifi54-253.clients.corp.yelpcorp.com>
2013-03-22 14:51:20 -0700

    Fixed some bugs.

--- util.py
+++ util.py
@@ -1,33 +1,34 @@
 import argparse
-from hackathon_portal import models
+from hackathon_portal import models, phonebook_etl, engrec_data_etl
 from hackathon_portal.testing import factories
+from sanitize_yelp_handles import SanitizeYelpHandler
 
 
 def parse_host_and_port():
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
             '--port',
             action='store',
             type=int,
             dest='port',
             default=5000,
             help='The port that should be listned on.',
-	)
-	parser.add_argument(
+    )
+    parser.add_argument(
             '--host',
             action='store',
             type=str,
             dest='host',
             default='0.0.0.0',
             help='The IP address on which to run the server.'
-	)
-	namespace = parser.parse_args()
-	return namespace.host, namespace.port
+    )
+    namespace = parser.parse_args()
+    return namespace.host, namespace.port
 
 
 def reset_tables():
-	models.db.drop_all()
-	models.db.create_all()
+    models.db.drop_all()
+    models.db.create_all()
 
 
 def reset_tables_and_build_hackathon_fixtures():
@@ -42,19 +43,27 @@ def fill_projects_with_default_photos():
     for project in hackathon.projects:
         if project.photos:
             continue
-	for project in models.Hackathon.query.filter(models.Hackathon.number == 7).one().projects:
-            if project.photos:
-                continue
-            image_url = image_fetcher.ImageFetcher(project.name).get_my_image()
-            _, extension = image_url.rsplit('.', 1)
-            photo = logic.add_photo(
-                project.name,
-                logic.download_image_from_url(image_url),
-                extension
-            )
+    for project in models.Hackathon.query.filter(models.Hackathon.number == 7).one().projects:
+        if project.photos:
+            continue
+        image_url = image_fetcher.ImageFetcher(project.name).get_my_image()
+        _, extension = image_url.rsplit('.', 1)
+        photo = logic.add_photo(
+            project.name,
+            logic.download_image_from_url(image_url),
+            extension
+        )
 
         logic.associate_photo_with_project(photo, project)
 
+def run_bootstrap():
+    #reset_tables()
+    #phone_book_etl = phonebook_etl.PhoneBookETL('data/phonebook.csv')
+    #phone_book_etl.execute()
+    #SanitizeYelpHandler()
+    data_loader_etl = engrec_data_etl.EngRecDataETL('data/hackathondata.csv')
+    data_loader_etl.execute()
+
 
 if __name__ == '__main__':
-    reset_tables()
+    run_bootstrap()
